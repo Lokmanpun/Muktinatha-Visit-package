d39df3f9e37b6ec75d10e62cbf86956a08fec104
Lokman <77951923+Lokmanpun@users.noreply.github.com>
2021-01-25 14:52:10 +0900

    Add files via upload

---
+++ new 1.bash
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Yarshagumba  </title>
+
+</head>
+
+<body bgColor="white">
+
+<h1> yarshagumba </h1>
+
+<img src="https://5.imimg.com/data5/EP/CA/HH/ANDROID-79955934/product-jpeg-500x500.jpg"/>
+<p>
+With the melting of the snows in the Himalayas, hordes of villagers of Nepal’s far western region trek up to the alpine pastures near the towering Himalayan peaks where they pitch up camps for an extended stay of almost eight weeks, braving the cold and harsh environment. They spend their days mostly on all fours, crawling through the shrub lands, digging with utmost care when they espy an unusual kind of mushroom called Yarsagumba. This, they brush gently with a toothbrush, and keep it in their pickers’ basket. They come in their thousands, lured by the high returns promised for finding this unusual herb. Experienced pickers can earn over 2,500 dollars each during a good season, five times more than the annual average earnings of a Nepali. <br/>
+Yarsagumba is said to be the most expensive herb in the world today, a kilo of it fetching more than 10,000 dollars in the international market. Apparently, pickers at the site get a dollar apiece which eventually inflates to 30 dollars apiece in the cities. In the old days too, Yarsagumba couldn’t be said to come cheap. Travellers have noted that in 1890, black, rotten specimens cost four times their weight in silver (Cooke, 1892). In 1990, Yarsagumba cost $700/kilo in the Chinese wholesale market (Hollobaugh, 1993).  So, what exactly is Yarsagumba and why is it in so much demand?
+<img src="https://new-img.patrika.com/upload/2020/09/28/kidaa12_6427892-m.jpg"/>
+<p>
+Yarsagumba (Cordyceps, or also Ophiocordyceps Saneness) is a most weird herb. One term often used—Chinese Caterpillar Fungus—is pretty descriptive of the species. So is dong chong xia cao (winter insect, summer grass) as Yarsagumba is often referred to in China. How is it formed? The caterpillar of a moth genus Thitarodes (Hepialus) lives underground in shrub lands of the Tibetan plateau and Himalayas (3000 m-5000 m) for almost five years before becoming a pupa. During its larva state, it is attacked by a fungus of the genus Ophiocordycipitaceae which kills the insect by filling its body cavity with mycelium. Once the weather gets warmer, mushrooms growing out of the caterpillar’s forehead emerge from the ground.
+</p>
+
+
+</body>
+</html>
